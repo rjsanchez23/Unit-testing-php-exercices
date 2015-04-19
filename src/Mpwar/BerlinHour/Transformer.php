f5b7577436e9d38515d816b15f7a5e68e95978ca
rjsanchez <rjsanchezestevez@gmail.com>
2015-04-19 13:11:15 +0200

    Test cases covered and code refactored

--- src/Mpwar/BerlinHour/Transformer.php
+++ src/Mpwar/BerlinHour/Transformer.php
@@ -7,41 +7,26 @@ namespace Mpwar\BerlinHour;
 final class Transformer
 {
 
-
     public function fromDigitalToBerlin($time)
     {
-        $time = explode(":", $time);
-        $hours = $time[0];
-        $minutes = $time[1];
-        $seconds = $time[2];
-        $result = [
-            'seconds' => '0',
-            'hours_5x' => '0000',
-            'hours_1x' => '0000',
-            'minutes_5x' => '00000000000',
-            'minutes_1x' => '0000',
-        ];
+        list($hours, $minutes, $seconds) = explode(":",$time);
 
-        if ($seconds % 2) {
-            $result['seconds'] = 1;
-        }
-
-        $hours_5x = $this->hoursToLamp(floor($hours/5));
-        $result["hours_5x"] = implode($hours_5x);
-
-        $hours_1x = $this->hoursToLamp(($hours % 5));
-        $result["hours_1x"] = implode(($hours_1x));
+        $result["seconds"]      = $this->lampsToStringConversion($seconds % 2, 1);
+        $result["hours_5x"]     = $this->lampsToStringConversion(floor($hours/5), 4);
+        $result["hours_1x"]     = $this->lampsToStringConversion(($hours % 5), 4);
+        $result["minutes_5x"]   = $this->lampsToStringConversion(floor($minutes/5), 11);
+        $result["minutes_1x"]   = $this->lampsToStringConversion(($minutes % 5), 4);
 
         return $result;
     }
-
-    private function hoursToLamp($hours){
-        $lamp = [0,0,0,0];
-        for($index = 0; $index < $hours; $index++){
+    private function lampsToStringConversion($lampsOn, $lampsNumber)
+    {
+        $lamp =  array_fill (0, $lampsNumber , 0);
+        for($index = 0; $index < $lampsOn; $index++){
 
             $lamp[$index] = 1;
         }
-        return $lamp;
+        return implode($lamp);
 
     }
 
--- test/MpwarTest/Unit/BerlinHour/TransformerTest.php
+++ test/MpwarTest/Unit/BerlinHour/TransformerTest.php
@@ -35,7 +35,7 @@ final class TransformerTest extends \PHPUnit_Framework_TestCase
     }
 
     /** @test */
-    public function shouldHaveThreeHoursOfValueFiveLampOn()
+    public function shouldHaveThreeLampsOfFirstRowOn()
     {
         $timeArray = [
             'hours_5x' => '1110',
@@ -44,11 +44,11 @@ final class TransformerTest extends \PHPUnit_Framework_TestCase
         $transformer = new Transformer;
         $result = $transformer->fromDigitalToBerlin('19:00:00');
 
-        $this->assertEquals($timeArray["hours_5x"], $result["hours_5x"], "the lamp is off when second is even");
+        $this->assertEquals($timeArray["hours_5x"], $result["hours_5x"], "Three lights should be on, when computing the value 19 for the hours");
     }
 
     /** @test */
-    public function shouldHaveAllHoursOfValueFiveLampOn()
+    public function shouldHaveAllTheLampsOfTheFrstRowOn()
     {
         $timeArray = [
             'hours_5x' => '1111',
@@ -57,11 +57,11 @@ final class TransformerTest extends \PHPUnit_Framework_TestCase
         $transformer = new Transformer;
         $result = $transformer->fromDigitalToBerlin('20:00:00');
 
-        $this->assertEquals($timeArray["hours_5x"], $result["hours_5x"], "the lamp is off when second is even");
+        $this->assertEquals($timeArray["hours_5x"], $result["hours_5x"], "All of the lamps of the first row should be on when the time is 20 or plus");
     }
 
     /** @test */
-    public function shouldHaveNoneHoursOfValueFiveLampOn()
+    public function shouldHaveAllTheLampsOfTheFirstRowOff()
     {
         $timeArray = [
             'hours_5x' => '0000',
@@ -70,11 +70,11 @@ final class TransformerTest extends \PHPUnit_Framework_TestCase
         $transformer = new Transformer;
         $result = $transformer->fromDigitalToBerlin('00:00:00');
 
-        $this->assertEquals($timeArray["hours_5x"], $result["hours_5x"], "the lamp is off when second is even");
+        $this->assertEquals($timeArray["hours_5x"], $result["hours_5x"], "All of the lights of the first row should go off when the time is 00 ");
     }
 
     /** @test */
-    public function shouldHaveThreeHoursOfValueOneLampOn()
+    public function shouldHaveTwoLampsOfTheSecondRowOn()
     {
         $timeArray = [
             'hours_1x' => '1100',
@@ -83,11 +83,11 @@ final class TransformerTest extends \PHPUnit_Framework_TestCase
         $transformer = new Transformer;
         $result = $transformer->fromDigitalToBerlin('17:00:00');
 
-        $this->assertEquals($timeArray["hours_1x"], $result["hours_1x"], "the lamp is off when second is even");
+        $this->assertEquals($timeArray["hours_1x"], $result["hours_1x"], "Two of the lamps of the second row should be on when the time is 17");
     }
 
     /** @test */
-    public function shouldHaveAllHoursOfValueOneLampOn()
+    public function shouldHaveAllOfTheLampsOfTheSecondRowOn()
     {
         $timeArray = [
             'hours_1x' => '1111',
@@ -96,11 +96,11 @@ final class TransformerTest extends \PHPUnit_Framework_TestCase
         $transformer = new Transformer;
         $result = $transformer->fromDigitalToBerlin('24:00:00');
 
-        $this->assertEquals($timeArray["hours_1x"], $result["hours_1x"], "the lamp is off when second is even");
+        $this->assertEquals($timeArray["hours_1x"], $result["hours_1x"], "All of the lamps of the second row should be on when the time is 24");
     }
 
     /** @test */
-    public function shouldHaveNoneHoursOfValueOneLampOn()
+    public function shouldHaveAllOfTheLAmpsOfTheSecondRowOff()
     {
         $timeArray = [
             'hours_1x' => '0000',
@@ -109,6 +109,88 @@ final class TransformerTest extends \PHPUnit_Framework_TestCase
         $transformer = new Transformer;
         $result = $transformer->fromDigitalToBerlin('00:00:00');
 
-        $this->assertEquals($timeArray["hours_1x"], $result["hours_1x"], "the lamp is off when second is even");
+        $this->assertEquals($timeArray["hours_1x"], $result["hours_1x"], "All of the lamps of the second row should be off when the time is 00");
     }
+
+
+    /** @test */
+    public function shouldHaveFiveLampsOfTheThirdRowOn()
+    {
+        $timeArray = [
+            'minutes_5x' => '11111000000',
+        ];
+
+        $transformer = new Transformer;
+        $result = $transformer->fromDigitalToBerlin('19:25:00');
+
+        $this->assertEquals($timeArray["minutes_5x"], $result["minutes_5x"], "Five lamps of the third row should be on when the minutes are 25");
+    }
+
+    /** @test */
+    public function shouldHaveAllOfTheLampsOfTheThirdRowOn()
+    {
+        $timeArray = [
+            'minutes_5x' => '11111111111',
+        ];
+
+        $transformer = new Transformer;
+        $result = $transformer->fromDigitalToBerlin('20:57:00');
+
+        $this->assertEquals($timeArray["minutes_5x"], $result["minutes_5x"], "All of the lamps of the third row should go on when minutes are 55+");
+    }
+
+    /** @test */
+    public function shouldHaveAllOfTheLampsOfTheThirdRowOff()
+    {
+        $timeArray = [
+            'minutes_5x' => '00000000000',
+        ];
+
+        $transformer = new Transformer;
+        $result = $transformer->fromDigitalToBerlin('00:00:00');
+
+        $this->assertEquals($timeArray["minutes_5x"], $result["minutes_5x"], "All should be off on the third line when minutes are 00");
+    }
+
+    /** @test */
+    public function shouldHaveTwoLampsOfTheFourthRowOn()
+    {
+        $timeArray = [
+            'minutes_1x' => '1100',
+        ];
+
+        $transformer = new Transformer;
+        $result = $transformer->fromDigitalToBerlin('17:57:00');
+
+        $this->assertEquals($timeArray["minutes_1x"], $result["minutes_1x"], "When minutes are 57 the fourth row should have two lamps on");
+    }
+
+    /** @test */
+    public function shouldHaveAllHoursOfValueOneLampOn()
+    {
+        $timeArray = [
+            'minutes_1x' => '1111',
+        ];
+
+        $transformer = new Transformer;
+        $result = $transformer->fromDigitalToBerlin('24:59:00');
+
+        $this->assertEquals($timeArray["minutes_1x"], $result["minutes_1x"], "All of the lamps of the forth row should be on when minutes are 59");
+    }
+
+    /** @test */
+    public function shouldHaveAllOfTheLampsOfTheFourthRowOff()
+    {
+        $timeArray = [
+            'minutes_1x' => '0000',
+        ];
+
+        $transformer = new Transformer;
+        $result = $transformer->fromDigitalToBerlin('00:00:00');
+
+        $this->assertEquals($timeArray["minutes_1x"], $result["minutes_1x"], "All of the lamps of the forth row should be off when minutes are 00");
+    }
+
+
+
 }
