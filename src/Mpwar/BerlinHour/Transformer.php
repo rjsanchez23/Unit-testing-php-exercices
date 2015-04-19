5100631cb8cf50b1c3695d515f02fc5ccff1341a
rjsanchez <rjsanchezestevez@gmail.com>
2015-04-19 12:14:01 +0200

    Hours of value five test

--- src/Mpwar/BerlinHour/Transformer.php
+++ src/Mpwar/BerlinHour/Transformer.php
@@ -4,15 +4,44 @@
 namespace Mpwar\BerlinHour;
 
 
-final class Transformer {
+final class Transformer
+{
 
-    public function fromDigitalToBerlin($time){
-        $time = explode($time, ":");
-        if( $seconds % 2 == 0){
-            return 1;
-        } else {
-            return 0;
+
+    public function fromDigitalToBerlin($time)
+    {
+        $time = explode(":", $time);
+        $hours = $time[0];
+        $minutes = $time[1];
+        $seconds = $time[2];
+        $result = [
+            'seconds' => '0',
+            'hours_5x' => '0000',
+            'hours_1x' => '0000',
+            'minutes_5x' => '00000000000',
+            'minutes_1x' => '0000',
+        ];
+
+        if ($seconds % 2) {
+            $result['seconds'] = 1;
         }
+
+        $hours_5x = $this->hoursToLamp(floor($hours/5));
+        $result["hours_5x"] = implode($hours_5x);
+
+        return $result;
     }
+
+    private function hoursToLamp($hours){
+        $lamp = [0,0,0,0];
+        for($index = 0; $index < $hours; $index++){
+
+            $lamp[$index] = 1;
+        }
+        return $lamp;
+
+    }
+
+
 
 }
--- test/MpwarTest/Unit/BerlinHour/TransformerTest.php
+++ test/MpwarTest/Unit/BerlinHour/TransformerTest.php
@@ -8,20 +8,68 @@ use Mpwar\BerlinHour\Transformer;
 
 final class TransformerTest extends \PHPUnit_Framework_TestCase
 {
-    /**@test*/
+    /** @test */
     public function shouldBeOdd()
     {
         $timeArray = [
-            'seconds'    => '0',
-            'hours_5x'   => '0000',
-            'hours_1x'   => '0000',
-            'minutes_5x' => '00000000000',
-            'minutes_1x' => '0000',
+            'seconds' => '0',
+        ];
+
+        $transformer = new Transformer;
+        $result = $transformer->fromDigitalToBerlin('05:32:58');
+
+        $this->assertEquals($timeArray["seconds"], $result["seconds"], "the lamp is off when second is odd");
+    }
+
+    /** @test */
+    public function shouldBeEven()
+    {
+        $timeArray = [
+            'seconds' => '1',
+        ];
+
+        $transformer = new Transformer;
+        $result = $transformer->fromDigitalToBerlin('05:32:59');
+
+        $this->assertEquals($timeArray["seconds"], $result["seconds"], "the lamp is off when second is even");
+    }
+
+    /** @test */
+    public function shouldHaveThreeHoursOfValueFiveLampOn()
+    {
+        $timeArray = [
+            'hours_5x' => '1110',
+        ];
+
+        $transformer = new Transformer;
+        $result = $transformer->fromDigitalToBerlin('19:00:00');
+
+        $this->assertEquals($timeArray["hours_5x"], $result["hours_5x"], "the lamp is off when second is even");
+    }
+
+    /** @test */
+    public function shouldHaveAllHoursOfValueFiveLampOn()
+    {
+        $timeArray = [
+            'hours_5x' => '1111',
+        ];
+
+        $transformer = new Transformer;
+        $result = $transformer->fromDigitalToBerlin('20:00:00');
+
+        $this->assertEquals($timeArray["hours_5x"], $result["hours_5x"], "the lamp is off when second is even");
+    }
+
+    /** @test */
+    public function shouldHaveNoneHoursOfValueFiveLampOn()
+    {
+        $timeArray = [
+            'hours_5x' => '0000',
         ];
 
         $transformer = new Transformer;
         $result = $transformer->fromDigitalToBerlin('00:00:00');
 
-        $this->assertEquals($timeArray["seconds"], $result["seconds"], "the lamp is off when second is odd");
+        $this->assertEquals($timeArray["hours_5x"], $result["hours_5x"], "the lamp is off when second is even");
     }
 }
