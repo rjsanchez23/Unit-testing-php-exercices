3efac18c354aaf9ea8e69813132c03a5c590994f
rjsanchez <rjsanchezestevez@gmail.com>
2015-04-29 19:29:50 +0200

    Test that player one should win by 15

--- src/Mpwar/Tennis/Match.php
+++ src/Mpwar/Tennis/Match.php
@@ -8,12 +8,18 @@ class Match {
 
     const PLAYER_1 = "p1";
     const PLAYER_2 = "p2";
+    private $player1Points = 0;
+    private $player2Points = 0;
 
 
-
-    public function score()
+    public function score($player)
     {
-
+        if($player == self::PLAYER_1){
+            $this->player1Points = 15;
+        }else{
+            $this->player2Points = 15;
+        }
+        return self::PLAYER_1 ." ". $this->player1Points . " - " . self::PLAYER_2 ." ". $this->player2Points;
     }
 
 }
--- test/MpwarTest/Unit/Tennis/TennisTest.php
+++ test/MpwarTest/Unit/Tennis/TennisTest.php
@@ -6,12 +6,17 @@ namespace MpwarTest\SetUp;
 
 use Mpwar\Tennis\Match;
 
-class TennisTest extends \PHPUnit_Framework_TestCase{
+class TennisTest extends \PHPUnit_Framework_TestCase
+{
 
     /** @test */
-    public function shouldSuccessIfFirstPlayerWinsWithFiveTeen(){
+    public function shouldSuccessIfFirstPlayerWinsWithFiveTeen()
+    {
 
-       
+        $match = new Match();
+        $result = $match->score(Match::PLAYER_1);
+        $spectedResult = 'p1 15 - p2 0';
+        $this->assertEquals($spectedResult, $result, "The espectected result is that player 1 wins by 15" );
 
     }
 
