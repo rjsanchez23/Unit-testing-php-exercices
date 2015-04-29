9b04370bd9cf8e93a46fba1ac0b04dda76f9d298
rjsanchez <rjsanchezestevez@gmail.com>
2015-04-29 19:22:27 +0200

    empty tests

---
+++ src/Mpwar/Tennis/Match.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Mpwar\Tennis;
+
+
+class Match {
+
+    const PLAYER_1 = "p1";
+    const PLAYER_2 = "p2";
+
+
+
+    public function score()
+    {
+
+    }
+
+}
---
+++ test/MpwarTest/Unit/Tennis/TennisTest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace MpwarTest\SetUp;
+
+
+use Mpwar\Tennis\Match;
+
+class TennisTest extends \PHPUnit_Framework_TestCase{
+
+    /** @test */
+    public function shouldSuccessIfFirstPlayerWinsWithFiveTeen(){
+
+       
+
+    }
+
+}
